febba04d6245f7cba24f664ba36e46a81313f5e9
chaiinchomp <chaiinchomp@users.noreply.github.com>
2020-08-27 15:10:01 -0700

    Improve news card layout

--- src/components/NewsEntry.js
+++ src/components/NewsEntry.js
@@ -1,20 +1,48 @@
 import React, { useState } from "react";
-import { Jumbotron, Button, Collapse } from "react-bootstrap";
+import { Button, Card, Media } from "react-bootstrap";
 
-export default function NewsEntry({ title, thumbnail, htmlContent }) {
+export default function NewsEntry({ title, thumbnail, leadText, htmlContent }) {
     const [expanded, setExpanded] = useState(false);
 
     return (
-        <Jumbotron className="bg-dark text-white">
-            <h1>{title}</h1>
-            <Button
-                onClick={() => setExpanded(!expanded)}
-            >Read more</Button>
-
-            <Collapse in={expanded}>
-                <div>{htmlContent}</div>
-            </Collapse>
-
-        </Jumbotron>
+        <Card bg='dark' key={title} text='white' style={{ margin: '1rem' }} >
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                    <Media>
+                        {thumbnail && <img
+                            width='auto'
+                            height={100}
+                            className="mr-3"
+                            src={thumbnail}
+                            alt={title}
+                        />}
+                        <Media.Body>
+                            {expanded ? (
+                                <React.Fragment>
+                                    <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+                                    <Button
+                                        variant='link'
+                                        onClick={() => setExpanded(!expanded)}
+                                    >
+                                        Show less
+                                    </Button>
+                                </React.Fragment>
+                            ) : (
+                                    <p>
+                                        <div dangerouslySetInnerHTML={{ __html: leadText + '...' }} />
+                                        <Button
+                                            variant='link'
+                                            onClick={() => setExpanded(!expanded)}
+                                        >
+                                            Read more
+                                    </Button>
+                                    </p>
+                                )}
+                        </Media.Body>
+                    </Media>
+                </Card.Text>
+            </Card.Body>
+        </Card>
     );
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -8,11 +8,11 @@ export default function Homepage() {
                 width: "80%",
                 marginLeft: "auto",
                 marginRight: "auto",
-                marginTop: "20px"
             }}
             className="content"
         >
-            <NewsEntry title={'Test Entry'} htmlContent={'lorem ipsum whatever blah blah blah here is some <h2>inline</h2> <b>html</b> to see if that works'} />
+            <NewsEntry title={'Test Entry'} leadText={'lorem ipsum whatever blah blah blah here'} htmlContent={'lorem ipsum whatever blah blah blah here is some <h2>inline</h2> <b>html</b> to see if that works'} thumbnail={'https://oldschool.runescape.wiki/images/a/a8/Leagues_II_-_Trailblazer_%281%29.png?2d320'} />
+            <NewsEntry title={'Test Entry 2'} leadText={'this text is extra long'} htmlContent={'this text is extra long so long wow here we go do you want some lOrEm iPsUm cause YOU GOT IT BRO HERE WE GO Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'} />
         </div>
     );
 }
