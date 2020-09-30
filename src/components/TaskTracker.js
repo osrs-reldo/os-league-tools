1cd16f93da13e94ff118efda0a677c8720591efb
chaiinchomp <chaiinchomp@users.noreply.github.com>
2020-09-30 12:57:33 -0700

    Tweak task list UI

--- src/components/TaskTracker.js
+++ src/components/TaskTracker.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
-import filterFactory, { textFilter, selectFilter, numberFilter } from 'react-bootstrap-table2-filter';
+import filterFactory, { textFilter, selectFilter } from 'react-bootstrap-table2-filter';
 import paginationFactory from 'react-bootstrap-table2-paginator';
-import { Card, Container, Row, Col, CardDeck } from "react-bootstrap";
+import { Card, Container, Row, Col, Tabs, Tab, Nav } from "react-bootstrap";
 import useLocalStorage from "../hooks/useLocalStorage";
 import { LOCALSTORAGE_KEYS } from '../util/constants';
 import taskData from '../resources/taskData.json';
@@ -40,15 +40,6 @@ export default function TaskTracker() {
             "filter": selectFilter({
                 "placeholder": "(all)",
                 "options": taskData.difficulties
-            })
-        },
-        {
-            "dataField": "area",
-            "text": "Area",
-            "sort": true,
-            "filter": selectFilter({
-                "placeholder": "(all)",
-                "options": taskData.areas
             })
         },
         {
@@ -117,20 +108,21 @@ export default function TaskTracker() {
 
     return (
         <Card bg='dark' text='white' className="mt-3">
-            <h1 className="m-3 text-center">Tasks Completed: 100 / 1000 (10%)</h1>
-
-            <Container style={{ 'maxWidth': '35rem' }}>
+            <Container >
                 <Row>
-                    <Col>
-                        By difficulty:
+                    <Col className="mt-1 text-center">
                         <ul>
                             <li>Easy: 60 / 500 (12%)</li>
                             <li>Medium: 30 / 300 (10%)</li>
                             <li>Hard: 5 / 200 (2%)</li>
+                            <li>Elite: 0 / 200 (0%)</li>
+                            <li>Master: 0 / 200 (0%)</li>
                         </ul>
                     </Col>
                     <Col>
-                        By area:
+                        <h1 className="m-3 text-center">Tasks Completed: 100 / 1000 (10%)</h1>
+                    </Col>
+                    <Col className="mt-1 text-center">
                         <ul>
                             <li>Misthalin: 70 / 250 (28%)</li>
                             <li>Karamja: 15 / 250 (6%)</li>
@@ -141,50 +133,69 @@ export default function TaskTracker() {
                     </Col>
                 </Row>
             </Container>
-            <div className='d-flex justify-content-around' style={{ margin: '1rem' }}>
-                <Card bg='dark' text='white' style={{ width: '30rem', marginRight: '1rem' }}>
-                    <h3 className="mt-2 light-text text-center">To-Do List</h3>
-                    <div className="m-3 text-center">
-                        <BootstrapTable
-                            bootstrap4
-                            keyField='id'
-                            data={tempTaskData.tasks}
-                            columns={todoColumns}
-                            bordered={false}
-                            classes="light-text"
-                            filter={filterFactory()}
-                            filterPosition="top"
-                        // pagination={paginationFactory({
-                        //     pageButtonRenderer,
-                        //     sizePerPage: 20,
-                        //     hideSizePerPage: true,
-                        //     hidePageListOnlyOnePage: true
-                        // })}
-                        />
-                    </div>
-                </Card>
-                <Card bg='dark' text='white' style={{ width: '60rem' }}>
-                    <h3 className="mt-2 light-text text-center">All Tasks</h3>
-                    <div className="m-3 text-center">
-                        <BootstrapTable
-                            bootstrap4
-                            keyField='id'
-                            data={taskData.tasks}
-                            columns={columns}
-                            bordered={false}
-                            classes="light-text"
-                            filter={filterFactory()}
-                            filterPosition="top"
-                            pagination={paginationFactory({
-                                pageButtonRenderer,
-                                sizePerPage: 20,
-                                hideSizePerPage: true,
-                                hidePageListOnlyOnePage: true
-                            })}
-                        />
-                    </div>
-                </Card>
-            </div>
+            <Tabs fill defaultActiveKey={"all"} className="pl-3 pr-3 pb-2 tab-bar-secondary">
+                <Tab eventKey="all" title="All Tasks">
+                    <Card bg='dark' text='white' style={{ 'border': '2px solid #6c757d' }}>
+                        <div className="m-3 text-center">
+                            <Tab.Container defaultActiveKey="common">
+                                <Row>
+                                    <Col sm={2}>
+                                        <h5>Areas:</h5>
+                                        <Nav variant="pills" className="flex-column mt-3 tab-bar-secondary">
+                                            {taskData.areas.map(area =>
+                                                <Nav.Item>
+                                                    <Nav.Link eventKey={area}>{area}</Nav.Link>
+                                                </Nav.Item>
+                                            )}
+                                        </Nav>
+                                    </Col>
+                                    <Col sm={10}>
+                                        <Tab.Content>
+                                            {taskData.areas.map(area =>
+                                                <Tab.Pane eventKey={area}>
+                                                    <BootstrapTable
+                                                        bootstrap4
+                                                        keyField='id'
+                                                        data={taskData.tasks[area]}
+                                                        columns={columns}
+                                                        bordered={false}
+                                                        classes="light-text"
+                                                        filter={filterFactory()}
+                                                        filterPosition="top"
+                                                        pagination={paginationFactory({
+                                                            pageButtonRenderer,
+                                                            sizePerPage: 20,
+                                                            hideSizePerPage: true,
+                                                            hidePageListOnlyOnePage: true
+                                                        })}
+                                                    />
+                                                </Tab.Pane>
+                                            )}
+                                        </Tab.Content>
+                                    </Col>
+                                </Row>
+                            </Tab.Container>
+                        </div>
+                    </Card>
+                </Tab>
+                <Tab eventKey="todo" title="To-Do List">
+                    <Card bg='dark' text='white' style={{ 'border': '2px solid #6c757d' }}>
+                        <div className="m-3 text-center">
+                            <h3 className="mt-2 light-text text-center">To-Do List</h3>
+                            <BootstrapTable
+                                bootstrap4
+                                keyField='id'
+                                data={tempTaskData.tasks}
+                                columns={todoColumns}
+                                bordered={false}
+                                classes="light-text"
+                                filter={filterFactory()}
+                                filterPosition="top"
+                            />
+                        </div>
+                    </Card>
+                </Tab>
+            </Tabs>
         </Card>
     );
 }
