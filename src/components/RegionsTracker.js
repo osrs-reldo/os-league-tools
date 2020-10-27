88b251a0fa886efe5f61eed857518f1d16e1d635
chaiinchomp <chaiinchomp@users.noreply.github.com>
2020-10-27 16:24:19 -0700

    Replace placeholder region data with real data

--- src/components/RegionsTracker.js
+++ src/components/RegionsTracker.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, Container, Row, Col } from "react-bootstrap";
 import ProgressBar from "./UnlockProgressBar";
 import RelicCheckImg from '../resources/img/relic-check.png';
 import { MAX_TASKS, REGION_UNLOCKS } from '../util/region-util';
 import RegionMap from "./RegionMap";
+import regionsData from '../resources/regionsData.json';
 
 export default function RegionsTracker({totalTasks}) {
-
+    const [selectedRegion, setSelectedRegion] = useState("Misthalin");
 
     return (
         <Card bg='dark' text='white' className="mt-3">
@@ -24,165 +25,95 @@ export default function RegionsTracker({totalTasks}) {
                     </Col>
                     <Col className={'relic-table m-2'} sm={4}>
                         <div style={{ maxHeight: '600px', overflowY: 'scroll' }}>
-                            <h3>Misthalin</h3>
+                            <h3>{selectedRegion}</h3>
 
                             <h5>Overview:</h5>
-                            The Misthalin area covers the entire Kingdom of Misthalin along with Fossil Island, Zanaris and the Abyss.
-                            Instances of areas in Misthalin and underground areas with an entrance in Misthalin will also be included.
-                            Access to Al-Kharid, Port Sarim and the western exit of Barbarian Village are restricted and prevented through the use of magical barriers.
-                            All transportation and teleportation through Charter Ships, Customs Officers, Gnome Gliders, Fairy Rings, etc will require you to have unlocked access to whichever area the destination is within.
-                            <br /><br />
-                            The following travel options are restricted unless you have access to the appropriate area:
-                            <ul>
-                                <li>The gate between Lumbridge and Al-Kharid</li>
-                                <li>The grapple shortcut between Lumbridge and Al-Kharid</li>
-                                <li>The stepping stones between Lumbridge Swamp and the Kalphite Lair</li>
-                                <li>The under-wall shortcut between Draynor Village and Port Sarim</li>
-                                <li>The underground sections of the Paterdomus Temple</li>
-                                <li>The railing shortcut between the Paterdomus Temple and Canifis</li>
-                            </ul>
+                            {regionsData[selectedRegion].overview}
+
+                            {regionsData[selectedRegion].travel &&
+                                <React.Fragment>
+                                    <br /><br />
+                                    The following travel options are restricted unless you have access to the appropriate area:
+                                    <ul>
+                                        {regionsData[selectedRegion].travel.map(listItem => <li>{listItem}</li>)}
+                                    </ul>
+                                </React.Fragment>
+                            }
 
                             <h5>Key Info:</h5>
 
                             Notable Settlements:
                             <ul>
-                                <li>Barbarian Village</li>
-                                <li>Draynor Village</li>
-                                <li>Edgeville</li>
-                                <li>Fossil Island</li>
-                                <li>Lumbridge</li>
-                                <li>The Digsite</li>
-                                <li>Varrock</li>
-                                <li>Zanaris</li>
+                                {regionsData[selectedRegion].settlements.map(listItem => <li>{listItem}</li>)}
                             </ul>
 
                             Notable combat-related activities:
                             <ul>
-                                <li>Abyssal Sire</li>
-                                <li>Bryophyta</li>
-                                <li>Chaeldar Slayer Tasks</li>
-                                <li>Edgeville Dungeon</li>
-                                <li>Fossil Island Wyverns</li>
-                                <li>Lumbridge Swamp Caves</li>
-                                <li>Obor</li>
-                                <li>Stronghold of Security</li>
-                                <li>Vanakka Slayer Tasks</li>
+                                {regionsData[selectedRegion].combatActivities.map(listItem => <li>{listItem}</li>)}
                             </ul>
 
                             Notable non-combat activities:
                             <ul>
-                                <li>Champions' Guild</li>
-                                <li>Draynor Village Rooftop Agility Course</li>
-                                <li>Fossil Island Herbiboar</li>
-                                <li>Fossil Island Hardwoord Tree Patches</li>
-                                <li>Fossil Island Sulliusceps Woodcutting</li>
-                                <li>Fossil Island Volcanic Mine</li>
-                                <li>Puro-Puro Implings</li>
-                                <li>Cosmic, Earth and Water Altars</li>
-                                <li>Varrock Rooftop Agility Course</li>
+                                {regionsData[selectedRegion].nonCombatActivities.map(listItem => <li>{listItem}</li>)}
                             </ul>
+
+                            {regionsData[selectedRegion].shops &&
+                                <React.Fragment>
+                                    Notable shops:
+                                    <ul>
+                                        {regionsData[selectedRegion].shops.map(listItem => <li>{listItem}</li>)}
+                                    </ul>
+                                </React.Fragment>
+                            }
 
                             All Slayer Masters will assign tasks from the same Slayer list and assign the same number of Slayer Points on task completion.
                             Slayer tasks will still respect Slayer and Combat Level requirements.
                             <br /><br />
                             Unlocking Misthalin will add the following tasks to your Slayer list:
                             <ul>
-                                <li>Cows (while under level 50 Combat)</li>
-                                <li>Goblins (while under level 50 Combat)</li>
-                                <li>Minotaurs (while under level 50 Combat)</li>
-                                <li>Zombies (while under level 50 Combat)</li>
-                                <li>Abyssal Demons</li>
-                                <li>Ankou</li>
-                                <li>Fossil Island Wyverns</li>
-                                <li>Hill Giants</li>
-                                <li>Moss Giants</li>
-                                <li>Spiders</li>
-                                <li>Sourhogs (after completing a Porcine of Interest)</li>
+                                {regionsData[selectedRegion].slayerTasks.map(listItem => <li>{listItem}</li>)}
                             </ul>
 
                             <h5>Unlocks:</h5>
 
                             The following quests are automatically completed but will not reward any completion XP:
                             <ul>
-                                <li>Dragon Slayer</li>
-                                <li>Druidic Ritual</li>
-                                <li>Elemental Workshop I</li>
-                                <li>Tears of Guthix</li>
-                                <li>Bone Voyage Dig Site</li>
-                                <li>Fairytale II - Cure a Queen</li>
-                                <li>Fairytale I - Growing Pains</li>
-                                <li>Lost City</li>
-                                <li>Nature Spirit</li>
-                                <li>Priest in Peril</li>
-                                <li>The Restless Ghost</li>
+                                {regionsData[selectedRegion].questUnlocks.map(listItem => <li>{listItem}</li>)}
                             </ul>
 
-                            The following tasks in the Lumbridge & Draynor Achievement Diary are automatically completed.
-                            Easy Tasks:
-                            <ul>
-                                <li>Catch some Anchovies in Al-Kharid</li>
-                                <li>Mine some iron ore at the Al-Kharid mine</li>
-                            </ul>
-                            Medium Tasks:
-                            <ul>
-                                <li>Complete a lap of the Al-Kharid Rooftop Course</li>
-                                <li>Grapple across the River Lum</li>
-                                <li>Craft some lava runes at the fire altar in Al-Kharid</li>
-                                <li>Purchase an upgraded device from Ava</li>
-                            </ul>
-                            Hard Tasks:
-                            <ul>
-                                <li>Cast Bones to Peaches in Al-Kharid Palace</li>
-                                <li>Recharge your prayer at the Duel Arena with Smite activated</li>
-                            </ul>
-                            Elite Tasks:
-                            <ul>
-                                <li>Chop some magic logs at the magic training arena</li>
-                                <li>Steal from a Dorgesh-Kaan rich chest</li>
-                                <li>Pickpocket Movario on the Dorgesh-Kaan Agility Course</li>
-                                <li>Perform the Quest Point cape emote in the Wise Old Man's house</li>
-                            </ul>
-
-                            The following tasks in the Varrock Diary are automatically completed:
-                            Medium Tasks:
-                            <ul>
-                                <li>Select a colour for your kitten</li>
-                                <li>Use the Spirit tree in the north-eastern corner of Grand Exchange</li>
-                                <li>Pick a white tree fruit</li>
-                                <li>Use the balloon to travel from Varrock</li>
-                            </ul>
-                            Hard Tasks:
-                            <ul>
-                                <li>Trade furs with the Fancy Dress Seller for a Spottier cape and equip it</li>
-                                <li>Teleport to Paddewwa</li>
-                            </ul>
-                            Elite Tasks:
-                            <ul>
-                                <li>Use Lunar magic to make 20 mahogany planks in the Varrock Lumber Yard</li>
-                                <li>Smith and fletch ten rune darts within Varrock</li>
-                            </ul>
+                            {regionsData[selectedRegion].diaryUnlocks &&
+                                <React.Fragment>
+                                    {regionsData[selectedRegion].diaryUnlocks.map(diaryData => (
+                                        <React.Fragment>
+                                            The following tasks in the {diaryData.area} Achievement Diary are automatically completed.
+                                            <br /><br />
+                                            Easy Tasks:
+                                            <ul>
+                                                {diaryData.easyTasks.map(listItem => <li>{listItem}</li>)}
+                                            </ul>
+                                            Medium Tasks:
+                                            <ul>
+                                                {diaryData.medTasks.map(listItem => <li>{listItem}</li>)}
+                                            </ul>
+                                            Hard Tasks:
+                                            <ul>
+                                                {diaryData.hardTasks.map(listItem => <li>{listItem}</li>)}
+                                            </ul>
+                                            Elite Tasks:
+                                            <ul>
+                                                {diaryData.eliteTasks.map(listItem => <li>{listItem}</li>)}
+                                            </ul>
+                                        </React.Fragment>
+                                    ))}
+                                </React.Fragment>
+                            }
 
                             <h5>Drops:</h5>
 
                             The drops listed below are affected by the drop rate modifier perks granted by passive Relic unlocks.
-                            Drop rates affected are always rounded down and are not tied to area unlocks, this list represents the key drops affected in Misthalin.
+                            Drop rates affected are always rounded down and are not tied to area unlocks, this list represents the key drops affected in {selectedRegion}.
                             <ul>
-                                <li>Abyssal Whip from Abyssal Demons</li>
-                                <li>Unsired from Abyssal Sire</li>
-                                <li>Wyvern Visage from all sources</li>
-                                <li>Draconic Visage from all sources</li>
-                                <li>Dragon platelegs/plateskirts from all sources</li>
-                                <li>Dragon Limbs from all sources</li>
-                                <li>Dragon Metal Slice from Adamant dragons</li>
-                                <li>Dragon metal Lump from Rune dragons</li>
-                                <li>Hill Giant Club from Obor</li>
-                                <li>Bryophyta's Essence from Bryophyta</li>
-                                <li>Beginner clue unique items</li>
-                                <li>Easy clue unique items</li>
-                                <li>Medium clue unique items</li>
-                                <li>Hard clue unique items</li>
-                                <li>Elite clue unique items</li>
-                                <li>Master clue unique items</li>
+                                {regionsData[selectedRegion].notableDrops.map(listItem => <li>{listItem}</li>)}
                             </ul>
                         </div>
                     </Col>
