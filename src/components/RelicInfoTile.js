7ee2bc82685abe863689943f2d3b831efc8dd2d4
chaiinchomp <chaiinchomp@users.noreply.github.com>
2020-10-28 12:46:58 -0700

    Replace placeholder relic data

--- src/components/RelicInfoTile.js
+++ src/components/RelicInfoTile.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Col } from "react-bootstrap";
-import RelicOutlineImg from '../resources/img/relic-outline.png';
 import { getRelicInfo } from '../util/relic-util';
 
 export default function RelicInfoTile({ relicKey, selected, onClickEvent, isWide, additionalContent }) {
     const { name, description, icon } = getRelicInfo(relicKey);
 
+    if (!name) {
+        return (<Col className={'relic-table-cell p-2'}></Col>);
+    }
+
     if (isWide) {
         return (
             <Col className={'relic-table-cell p-2' + (selected ? ' selected' : '')} onClick={onClickEvent}>
-                <img src={RelicOutlineImg} alt='' height={75} width={75} className='float-left m-3' />
+                {icon && <img src={icon} alt='' height={75} width={75} className='float-left m-3' />}
                 <div>
                     <h5>{name}</h5>
-                    <p>{description}</p>
+                    <ul>
+                        {description.map(listItem => <li>{listItem}</li>)}
+                    </ul>
                     {additionalContent}
                 </div>
             </Col>
@@ -21,11 +26,11 @@ export default function RelicInfoTile({ relicKey, selected, onClickEvent, isWide
 
     return (
         <Col
-            className={'relic-table-cell p-2 d-flex flex-column align-items-center justify-content-center' + (selected ? ' selected' : '')}
+            className={'relic-table-cell p-2 d-flex flex-column align-items-center justify-content-center text-center' + (selected ? ' selected' : '')}
             onClick={onClickEvent}
         >
             <h5>{name}</h5>
-            {icon && <img src={RelicOutlineImg} alt='' height={75} width={75} />}
+            {icon && <img src={icon} alt='' height={75} width={75} />}
             {additionalContent}
         </Col>
     );
