cafe0fb81c9ec7ca30762ec4ea3e3e67fb3cc97c
chaiinchomp <chaiinchomp@users.noreply.github.com>
2020-10-31 09:36:48 -0700

    Fix some style gripes

--- src/components/TaskTracker.js
+++ src/components/TaskTracker.js
@@ -172,10 +172,10 @@ function TaskTableWrapper({
 
     let allFilters = [...taskFilters];
 
-    if ({selectedStatus}.selectedStatus === "Incomplete") {
+    if (selectedStatus === "Incomplete") {
         allFilters.push(showIncompleteFilter);
     }
-    if ({selectedStatus}.selectedStatus === "Complete") {
+    if (selectedStatus === "Complete") {
         allFilters.push(showCompleteFilter);
     }
     if (hideLockedAreas) {
@@ -185,76 +185,69 @@ function TaskTableWrapper({
     return (
         <Card bg='dark' text='white' style={{ border: '2px solid #6c757d', borderRadius: '0rem 0rem .25rem .25rem' }}>
             <div className="m-3 text-center">
-                <Nav>
-                    <Row>
-                        <Col sm={2}>
-                            <Tab.Container activeKey={selectedArea}>
-                                <Form.Check
-                                    label="Hide locked areas"
-                                    checked={hideLockedAreas}
-                                    onChange={() => setHideLockedAreas(prevHideLocked => !prevHideLocked)}
-                                />
-                                <div className="mt-2 mb-2" style={{borderTop: '0.5px solid', width: '100%'}} />
-                                <h5>Areas:</h5>
-                                <Nav variant="pills" className="flex-column mt-3 tab-bar-secondary">
-                                    <Nav.Item key='All'>
-                                        <Nav.Link eventKey='All' onClick={event => setSelectedArea('All')}>All</Nav.Link>
-                                    </Nav.Item>
-                                    {hideLockedAreas ?
-                                        unlockedRegions.map(area =>
-                                            <Nav.Item key={area}>
-                                                <Nav.Link eventKey={area} onClick={() => setSelectedArea(area)}>{area}</Nav.Link>
-                                            </Nav.Item>
-                                        )
-                                    :
-                                        taskData.areas.map(area =>
-                                            <Nav.Item key={area}>
-                                                <Nav.Link eventKey={area}>{area}</Nav.Link>
-                                            </Nav.Item>
-                                        )
-                                    }
-                                </Nav>
-                                <div className="mt-2 mb-2" style={{borderTop: '0.5px solid', width: '100%'}} />
-                            </Tab.Container>
-
-                            <Tab.Container activeKey={selectedStatus}>
-                                <h5>Status:</h5>
-                                <Nav variant="pills" className="flex-column mt-3 tab-bar-secondary">
-                                    <Nav.Item key='All'>
-                                        <Nav.Link eventKey='All' onClick={() => setSelectedStatus('All')}>All</Nav.Link>
-                                    </Nav.Item>
-                                    <Nav.Item key='Complete'>
-                                        <Nav.Link eventKey='Complete' onClick={() => {setSelectedStatus('Complete');
-                                                                                        
-                                                                                        
-                                                                                    }
-                                        }>Complete</Nav.Link>
-                                    </Nav.Item>
-                                    <Nav.Item key='Incomplete'>
-                                        <Nav.Link eventKey='Incomplete' onClick={() => setSelectedStatus('Incomplete')}>Incomplete</Nav.Link>
-                                    </Nav.Item>
-                                </Nav>
-                            </Tab.Container>
-                        </Col>
-
-                        <Col sm={10}>
-                            {plannedOnTodoList &&
-                                            <div className='d-flex justify-content-around'>
-                                                <h4 className="mb-3">
-                                                    Tasks on To-do List: {plannedOnTodoList.tasks}
-                                                </h4>
-                                                <h4 className="mb-3">
-                                                    Points on To-do List: {plannedOnTodoList.points}
-                                                </h4>
-                                            </div>
-                                        }
-                            <Nav>
-                                <TaskTable area={selectedArea} taskStatus={taskStatus} updateTaskStatusCallback={updateTaskStatusCallback} taskFilters={allFilters} />
+                <Row>
+                    <Col sm={2}>
+                        <Tab.Container activeKey={selectedArea}>
+                            <Form.Check
+                                label="Hide locked areas"
+                                checked={hideLockedAreas}
+                                onChange={() => setHideLockedAreas(prevHideLocked => !prevHideLocked)}
+                            />
+                            <div className="mt-2 mb-2" style={{borderTop: '0.5px solid', width: '100%'}} />
+                            <h5>Areas:</h5>
+                            <Nav variant="pills" className="flex-column mt-3 tab-bar-secondary">
+                                <Nav.Item key='All'>
+                                    <Nav.Link eventKey='All' onClick={event => setSelectedArea('All')}>All</Nav.Link>
+                                </Nav.Item>
+                                {hideLockedAreas ?
+                                    unlockedRegions.map(area =>
+                                        <Nav.Item key={area}>
+                                            <Nav.Link eventKey={area} onClick={() => setSelectedArea(area)}>{area}</Nav.Link>
+                                        </Nav.Item>
+                                    )
+                                :
+                                    taskData.areas.map(area =>
+                                        <Nav.Item key={area}>
+                                            <Nav.Link eventKey={area}>{area}</Nav.Link>
+                                        </Nav.Item>
+                                    )
+                                }
                             </Nav>
-                        </Col>
-                    </Row>
-                </Nav>
-                
+                            <div className="mt-2 mb-2" style={{borderTop: '0.5px solid', width: '100%'}} />
+                        </Tab.Container>
+
+                        <Tab.Container activeKey={selectedStatus}>
+                            <h5>Status:</h5>
+                            <Nav variant="pills" className="flex-column mt-3 tab-bar-secondary">
+                                <Nav.Item key='All'>
+                                    <Nav.Link eventKey='All' onClick={() => setSelectedStatus('All')}>All</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item key='Complete'>
+                                    <Nav.Link eventKey='Complete' onClick={() => setSelectedStatus('Complete')}>Complete</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item key='Incomplete'>
+                                    <Nav.Link eventKey='Incomplete' onClick={() => setSelectedStatus('Incomplete')}>Incomplete</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Tab.Container>
+                    </Col>
+
+                    <Col sm={10}>
+                        {plannedOnTodoList &&
+                            <div className='d-flex justify-content-around'>
+                                <h4 className="mb-3">
+                                    Tasks on To-do List: {plannedOnTodoList.tasks}
+                                </h4>
+                                <h4 className="mb-3">
+                                    Points on To-do List: {plannedOnTodoList.points}
+                                </h4>
+                            </div>
+                        }
+                        <Nav>
+                            <TaskTable area={selectedArea} taskStatus={taskStatus} updateTaskStatusCallback={updateTaskStatusCallback} taskFilters={allFilters} />
+                        </Nav>
+                    </Col>
+                </Row>
             </div>
         </Card>
     );
