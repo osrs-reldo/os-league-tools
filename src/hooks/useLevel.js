6e88cda206be8bde9f4be5c15395dc94c8adad06
chaiinchomp <chaiinchomp@users.noreply.github.com>
2020-11-01 11:41:36 -0800

    Fix not being able to backspace in calc exp/level inputs

--- src/hooks/useLevel.js
+++ src/hooks/useLevel.js
@@ -6,14 +6,24 @@ export default function useLevel(initialLevel) {
     const [exp, setExp] = useState(getExpForLevel(initialLevel));
 
     const updateByLevel = newLevel => {
-        newLevel = parseInt(newLevel);
-        setLevel(newLevel);
-        setExp(getExpForLevel(newLevel));
+        const parsedLevel = parseInt(newLevel);
+        if (isNaN(parsedLevel)) {
+            setLevel(newLevel);
+            setExp(0);
+        } else {
+            setLevel(parsedLevel);
+            setExp(getExpForLevel(parsedLevel));
+        }
     }
     const updateByExp = newExp => {
-        newExp = parseInt(newExp);
-        setExp(newExp);
-        setLevel(getLevelForExp(newExp));
+        const parsedExp = parseInt(newExp);
+        if (isNaN(parsedExp)) {
+            setExp(newExp);
+            setLevel(0);
+        } else {
+            setExp(parsedExp);
+            setLevel(getLevelForExp(parsedExp));
+        }
     }
 
     return { level, exp, updateByLevel, updateByExp };
