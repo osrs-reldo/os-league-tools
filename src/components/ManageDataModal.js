011b3e9683172f2d5d2ce9b0342a70573b15c85c
chaiinchomp <chaiinchomp@users.noreply.github.com>
2020-11-05 15:20:11 -0800

    Temp disable RL importer

--- src/components/ManageDataModal.js
+++ src/components/ManageDataModal.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
-import { Button, Modal, Alert, Tabs, Tab, InputGroup, FormControl } from "react-bootstrap";
+import { Button, Modal, Alert, Tabs, Tab } from "react-bootstrap";
 import { FilePicker } from "react-file-picker";
 import { saveLocalStorageToFile, loadLocalStorageFromFile } from "../util/file-util";
-import { updateLocalStorageFromRuneliteJson } from "../util/runelite-util";
+// import { updateLocalStorageFromRuneliteJson } from "../util/runelite-util";
 import { resetLocalStorageData } from "../util/browser-util";
 
 export default function ManageDataModal({ show, onClose }) {
     const [errorText, setErrorText] = useState("");
     const [successText, setSuccessText] = useState("");
     const [updated, setUpdated] = useState(false);
-    const [runeliteImportJson, setRuneliteImportJson] = useState({});
+    // const [runeliteImportJson, setRuneliteImportJson] = useState({});
 
     const pluginHubLink = "https://github.com/runelite/runelite/wiki/Information-about-the-Plugin-Hub";
 
@@ -33,19 +33,19 @@ export default function ManageDataModal({ show, onClose }) {
         setSuccessText("Data successfully deleted");
     }
 
-    const loadRuneliteImport = async () => {
-        const response = await updateLocalStorageFromRuneliteJson(runeliteImportJson);
-        if (response.success) {
-            setSuccessText(
-                "Successfully imported character data from Runelite."
-            );
-            setErrorText("");
-            setUpdated(true);
-        } else {
-            setSuccessText("");
-            setErrorText(response.message);
-        }
-    }
+    // const loadRuneliteImport = async () => {
+    //     const response = await updateLocalStorageFromRuneliteJson(runeliteImportJson);
+    //     if (response.success) {
+    //         setSuccessText(
+    //             "Successfully imported character data from Runelite."
+    //         );
+    //         setErrorText("");
+    //         setUpdated(true);
+    //     } else {
+    //         setSuccessText("");
+    //         setErrorText(response.message);
+    //     }
+    // }
 
     return (
         <Modal show={show} onHide={() => onClose(updated)}>
@@ -97,19 +97,19 @@ export default function ManageDataModal({ show, onClose }) {
                     </Tab>
                     <Tab eventKey="runelite" title="Runelite Import">
                         <div className="text-center mt-3">
-                            <Alert variant="info" className="small">
-                                The OsLeague plugin from the Runelite Plugin Hub is required.
+                            <Alert variant="danger" className="small">
+                                Importing from Runelite is temporarily disabled while we work through some bugs caused by the game update this morning.
                             </Alert>
-                            <p className="text-left">
+                            <p className="text-left" style={{textDecoration: 'line-through'}}>
                                 <h5>How to use:</h5>
                                 <ol>
                                     <li>Install the plugin using the <a href={pluginHubLink} target="_blank" rel="noopener noreferrer">plugin hub</a></li>
-                                    <li>Go to the Leagues tab ingame and open/close each interface to make sure your data is up-to-date</li>
+                                    <li>Go to the Leagues tab ingame and open/close each interface to make sure your data is up-to-date <b>NOTE: Make sure you have disabled all task filters on the ingame UI.</b></li>
                                     <li>Click on the OS League Tools icon on the top toolbar to copy your data</li>
                                     <li>Paste the copied text into the box below</li>
                                 </ol>
                             </p>
-                            <InputGroup>
+                            {/* <InputGroup>
                                 <InputGroup.Prepend>
                                     <InputGroup.Text>Paste copied data from Runelite:</InputGroup.Text>
                                 </InputGroup.Prepend>
@@ -124,7 +124,7 @@ export default function ManageDataModal({ show, onClose }) {
                             {errorText && <p className="text-danger small">{errorText}</p>}
                             {successText && (
                                 <p className="text-success small">{successText}</p>
-                            )}
+                            )} */}
                         </div>
                     </Tab>
                     <Tab eventKey="reset" title="Reset">
