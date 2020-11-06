5b6df80bcdf959b935184748ce86c243097ec4da
chaiinchomp <chaiinchomp@users.noreply.github.com>
2020-11-06 14:08:29 -0800

    Small screen improvements on homepage

--- src/components/BlogEntry.js
+++ src/components/BlogEntry.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Card, Media } from "react-bootstrap";
+import { Button, Card, Container, Row, Col } from "react-bootstrap";
 
 export default function NewsEntry({ title, date, thumbnail, leadText, htmlContent }) {
     const [expanded, setExpanded] = useState(false);
@@ -11,37 +11,41 @@ export default function NewsEntry({ title, date, thumbnail, leadText, htmlConten
                     <h4>{title}</h4>
                 </Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">{date}</Card.Subtitle>
-                <Media>
-                    {thumbnail && <img
-                        width='auto'
-                        height={100}
-                        src={thumbnail}
-                        alt={title}
-                    />}
-                    <Media.Body>
-                        {expanded ? (
-                            <React.Fragment>
-                                <div dangerouslySetInnerHTML={{ __html: htmlContent }} className="mb-1"  />
-                                <Button
-                                    variant="outline-light"
-                                    onClick={() => setExpanded(!expanded)}
-                                >
-                                    Show less
-                                </Button>
-                            </React.Fragment>
-                        ) : (
+                <Container fluid>
+                    <Row>
+                        <Col sm="auto">
+                            {thumbnail && <img
+                                width='auto'
+                                height={100}
+                                src={thumbnail}
+                                alt={title}
+                            />}
+                        </Col>
+                        <Col>
+                            {expanded ? (
                                 <React.Fragment>
-                                    <div dangerouslySetInnerHTML={{ __html: leadText + '...' }}  className="mb-1" />
+                                    <div dangerouslySetInnerHTML={{ __html: htmlContent }} className="mb-1"  />
                                     <Button
                                         variant="outline-light"
                                         onClick={() => setExpanded(!expanded)}
                                     >
-                                        Show more
+                                        Show less
                                     </Button>
                                 </React.Fragment>
+                            ) : (
+                                    <React.Fragment>
+                                        <div dangerouslySetInnerHTML={{ __html: leadText + '...' }}  className="mb-1" />
+                                        <Button
+                                            variant="outline-light"
+                                            onClick={() => setExpanded(!expanded)}
+                                        >
+                                            Show more
+                                        </Button>
+                                    </React.Fragment>
                             )}
-                    </Media.Body>
-                </Media>
+                        </Col>
+                    </Row>
+                </Container>
             </Card.Body>
         </Card>
     );
--- src/components/NewsEntry.js
+++ src/components/NewsEntry.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card, Media } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 
 export default function NewsEntry({ title, date, thumbnail, leadText, buttonLink, buttonText }) {
     return (
@@ -9,25 +9,29 @@ export default function NewsEntry({ title, date, thumbnail, leadText, buttonLink
                     <h4>{title}</h4>
                 </Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">{date}</Card.Subtitle>
-                <Media>
-                    {thumbnail && <img
-                        width='auto'
-                        height={100}
-                        src={thumbnail}
-                        alt={title}
-                        className='mr-3'
-                    />}
-                    <Media.Body>
-                        <div dangerouslySetInnerHTML={{ __html: leadText }} className="mb-2" />
-                        <Button
-                            variant="outline-light"
-                            as='a'
-                            href={buttonLink}
-                        >
-                            {buttonText}
-                        </Button>
-                    </Media.Body>
-                </Media>
+                <Container fluid>
+                    <Row>
+                        <Col sm="auto">
+                            {thumbnail && <img
+                                width='auto'
+                                height={100}
+                                src={thumbnail}
+                                alt={title}
+                                className='mr-3'
+                            />}
+                        </Col>
+                        <Col>
+                            <div dangerouslySetInnerHTML={{ __html: leadText }} className="mb-2" />
+                            <Button
+                                variant="outline-light"
+                                as='a'
+                                href={buttonLink}
+                            >
+                                {buttonText}
+                            </Button>
+                        </Col>
+                    </Row>
+                </Container>
             </Card.Body>
         </Card>
     );
