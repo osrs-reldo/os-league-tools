619092334e24f71751dfefb4c41036f3839d8413
chaiinchomp <chaiinchomp@users.noreply.github.com>
2020-11-27 18:10:11 -0800

    Add linter configs

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        indent: 'off',
+        "react/jsx-indent": 'off',
+        'react/prop-types': 'off',
+        'react/jsx-filename-extension': 'off',
+    },
+};
