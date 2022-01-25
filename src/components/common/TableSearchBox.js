9de2072119825523a896b1039249198c0657a9ad
DMills94 <davidj-mills@live.co.uk>
2022-01-25 14:40:04 +0000

    feat: Add a clear button on the table search input

--- src/components/common/TableSearchBox.js
+++ src/components/common/TableSearchBox.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 export default function SearchBox({ globalFilter, setGlobalFilter }) {
+    const inputRef = useRef(null);
+
+    const clearInput = () => {
+        setGlobalFilter('');
+        inputRef.current.focus();
+    };
+
     return (
-        <input
-            type='text'
-            className='input-primary form-input text-xs'
-            placeholder='Search...'
-            value={globalFilter || ''}
-            onChange={e => {
-                setGlobalFilter(e.target.value);
-            }}
-        />
+        <div className='relative'>
+            <input
+                ref={inputRef}
+                type='text'
+                className='input-primary form-input text-xs'
+                placeholder='Search...'
+                value={globalFilter || ''}
+                onChange={e => {
+                    setGlobalFilter(e.target.value);
+                }}
+            />
+            {!!globalFilter && (
+                <button
+                    type='button'
+                    className='h-full absolute right-0 top-0 cursor-pointer text-secondary-alt px-2 text-xs'
+                    onClick={clearInput}
+                >
+                    X
+                </button>
+            )}
+        </div>
     );
 }
