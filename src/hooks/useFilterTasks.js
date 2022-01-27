4fa9914d3fd6740917f46bf958020c29f41eb284
DMills94 <davidj-mills@live.co.uk>
2022-01-27 14:43:59 +0000

    feat: TaskGenerator uses filtered task view - Add state for no completeable tasks in filtered view - Extract filters into separate files and add useFilterTasks hook

---
+++ src/hooks/useFilterTasks.js
@@ -0,0 +1,10 @@
+import TASKS from "../data/tasks";
+import ALL_FILTERS from "../util/taskFilters";
+
+export default function useFilterTasks({ filters = ALL_FILTERS, filterState, tasks = TASKS, tasksState }) {
+  const filteredTasks = Object.values(tasks).filter(task =>
+      Object.values(filters).every(filter => filter(task, filterState, { tasksState }))
+  );
+
+  return Object.values(filteredTasks);
+};
