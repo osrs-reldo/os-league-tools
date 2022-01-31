94835557204866ec745221179844907e9fd10c56
DMills94 <davidj-mills@live.co.uk>
2022-01-31 23:13:53 +0000

    version update function for unlocks v4

--- src/store/unlocks/constants.js
+++ src/store/unlocks/constants.js
@@ -2,6 +2,16 @@ export const CURRENT_VERSION = 4;
 
 export const DEFAULT_UNLOCKED_SKILLS = ['Defence', 'Thieving', 'Fishing'];
 
+export const DEFAULT_UNLOCKED_BOSSES = [
+    'Callisto',
+    'Chaos Elemental',
+    'Chaos Fanatic',
+    'Crazy Archaeologist',
+    'Scorpia',
+    'Venenatis',
+    "Vet'ion",
+];
+
 export const INITIAL_STATE = {
     version: CURRENT_VERSION,
     skills: DEFAULT_UNLOCKED_SKILLS,
