1a7ac78a63e6c2fb4d15734a490f199a95ac662d
DMills94 <davidj-mills@live.co.uk>
2022-01-31 15:21:25 +0000

    feat: Wire in hiscores to boss panel and add renown costs

--- src/store/unlocks/constants.js
+++ src/store/unlocks/constants.js
@@ -1,20 +1,10 @@
-export const CURRENT_VERSION = 3;
+export const CURRENT_VERSION = 4;
 
 export const DEFAULT_UNLOCKED_SKILLS = ['Defence', 'Thieving', 'Fishing'];
-
-export const DEFAULT_UNLOCKED_BOSSES = [
-    'Callisto',
-    'Chaos Elemental',
-    'Chaos Fanatic',
-    'Crazy Archaeologist',
-    'Scorpia',
-    'Venenatis',
-    "Vet'ion",
-];
 
 export const INITIAL_STATE = {
     version: CURRENT_VERSION,
     skills: DEFAULT_UNLOCKED_SKILLS,
-    bosses: DEFAULT_UNLOCKED_BOSSES,
+    bosses: [],
     quests: {},
 };
