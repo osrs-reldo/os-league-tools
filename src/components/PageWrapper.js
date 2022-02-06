79e39dbbefc7f9530762c99ee3b84a4a6e83bd09
chaiinchomp <chaiinchomp@gmail.com>
2022-02-06 08:46:14 -0800

    Refactor navbar item components

--- src/components/PageWrapper.js
+++ src/components/PageWrapper.js
@@ -1,14 +1,14 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Page from './common/Page';
 import NavBar, { NavItem } from './common/NavBar';
-import AuthButton from './AuthButton';
+import AuthButton from './nav/AuthButton';
 import FeedbackModal from './FeedbackModal';
-import Dropdown from './common/Dropdown';
 import ManageDataModal from './ManageDataModal';
 import images from '../assets/images';
-import useClickListener from '../hooks/useClickListener';
 import useQueryString from '../hooks/useQueryString';
+import ManageData from './nav/ManageData';
+import Feedback from './nav/Feedback';
 
 export default function PageWrapper({ children }) {
     const theme = useSelector(state => state.settings.theme);
@@ -21,69 +21,20 @@ export default function PageWrapper({ children }) {
         new NavItem('Trackers', 'primary', 0, 1).withRouterLink('/tracker').withIconFont('checklist_rtl'),
         new NavItem('Calculators', 'primary', 0, 2).withRouterLink('/calculators').withIconFont('calculate'),
         new NavItem('Import', 'secondary', 1, 0).withCustomRenderFn(
-            () => <ManageDataDropdown setManageDataModalType={setManageDataModalType} />,
-            () => (
-                <React.Fragment key='dataManagement'>
-                    <button
-                        key='import'
-                        className='text-primary bg-hover py-1 text-left'
-                        onClick={() => setManageDataModalType('import')}
-                        type='button'
-                    >
-                        <span className='text-primary-alt icon-lg inline align-middle mr-1'>file_download</span>
-                        <p className='h-4 inline pl-1 font-sans-alt'>Import / Sync</p>
-                    </button>
-                    <button
-                        key='export'
-                        className='text-primary bg-hover py-1 text-left'
-                        onClick={() => setManageDataModalType('export')}
-                        type='button'
-                    >
-                        <span className='text-primary-alt icon-lg inline align-middle mr-1'>file_upload</span>
-                        <p className='h-4 inline pl-1 font-sans-alt'>Export data</p>
-                    </button>
-                    <button
-                        key='reset'
-                        className='text-primary bg-hover py-1 text-left'
-                        onClick={() => setManageDataModalType('reset')}
-                        type='button'
-                    >
-                        <span className='text-primary-alt icon-lg inline align-middle mr-1'>dangerous</span>
-                        <p className='h-4 inline pl-1 font-sans-alt'>Reset all data</p>
-                    </button>
-                </React.Fragment>
-            )
+            () => <ManageData.NavBarItem key='manage' setManageDataModalType={setManageDataModalType} />,
+            () => <ManageData.CollapsedMenu key='manage' setManageDataModalType={setManageDataModalType} />
         ),
         new NavItem('Login', 'secondary', 2, 0).withCustomRenderFn(
-            () => <AuthButton key='login' />,
-            () => <AuthButton key='login' useDropdownVariant />
+            () => <AuthButton.NavBarItem key='login' />,
+            () => <AuthButton.CollapsedMenu key='login' />
         ),
         new NavItem('Settings', 'overflow', 2, 1).withRouterLink('/settings').withIconFont('settings'),
         new NavItem('Discord', 'overflow', 3, 0)
             .withHref('https://discord.gg/GQ5kVyU', '_blank')
             .withIconFont('discord'),
         new NavItem('Feedback', 'overflow', 3, 1).withCustomRenderFn(
-            () => (
-                <Dropdown.Button
-                    key='feedback'
-                    icon='pest_control'
-                    className='text-left'
-                    onClick={() => setFeedbackModalOpen(true)}
-                >
-                    Feedback
-                </Dropdown.Button>
-            ),
-            () => (
-                <button
-                    key='feedback'
-                    className='text-primary bg-hover py-1 text-left'
-                    onClick={() => setFeedbackModalOpen(true)}
-                    type='button'
-                >
-                    <span className='text-primary-alt icon-lg inline align-middle mr-1'>pest_control</span>
-                    <p className='h-4 inline pl-1 font-sans-alt'>Feedback</p>
-                </button>
-            )
+            () => <Feedback.NavBarItem key='feedback' setFeedbackModalOpen={setFeedbackModalOpen} />,
+            () => <Feedback.CollapsedMenu key='feedback' setFeedbackModalOpen={setFeedbackModalOpen} />
         ),
         new NavItem('Github', 'overflow', 3, 2)
             .withHref('https://github.com/osrs-reldo/os-league-tools', '_blank')
@@ -109,39 +60,5 @@ export default function PageWrapper({ children }) {
                 />
             </Page.Body>
         </Page>
-    );
-}
-
-function ManageDataDropdown({ setManageDataModalType }) {
-    const [isExpanded, setExpanded] = useState(false);
-    const menuRef = useRef(null);
-    useClickListener(menuRef, () => setExpanded(false), true);
-
-    return (
-        <div className='relative'>
-            <button
-                key='plugin'
-                className='text-primary md:inline hidden navbar-link-alt bg-hover py-1 px-2'
-                type='button'
-                onClick={() => setExpanded(true)}
-            >
-                Manage Data
-            </button>
-            <div className='mt-1 absolute text-center'>
-                <Dropdown show={isExpanded} innerRef={menuRef}>
-                    <Dropdown.Separator />
-                    <Dropdown.Button className='text-left' onClick={() => setManageDataModalType('import')}>
-                        <span className='icon-base mr-1 align-bottom'>file_download</span> Import
-                    </Dropdown.Button>
-                    <Dropdown.Button className='text-left' onClick={() => setManageDataModalType('export')}>
-                        <span className='icon-base mr-1 align-bottom'>file_upload</span> Export
-                    </Dropdown.Button>
-                    <Dropdown.Separator />
-                    <Dropdown.Button className='text-left' onClick={() => setManageDataModalType('reset')}>
-                        <span className='icon-outline text-base mr-1 align-bottom'>dangerous</span> Reset
-                    </Dropdown.Button>
-                </Dropdown>
-            </div>
-        </div>
     );
 }
---
+++ src/components/nav/ManageData.js
@@ -0,0 +1,72 @@
+import React, { useRef, useState } from 'react';
+import useClickListener from '../../hooks/useClickListener';
+import Dropdown from '../common/Dropdown';
+
+function NavBarItem({ setManageDataModalType }) {
+    const [isExpanded, setExpanded] = useState(false);
+    const menuRef = useRef(null);
+    useClickListener(menuRef, () => setExpanded(false), true);
+
+    return (
+        <div className='relative'>
+            <button
+                className='text-primary md:inline hidden navbar-link-alt bg-hover py-1 px-2'
+                type='button'
+                onClick={() => setExpanded(true)}
+            >
+                Manage Data
+            </button>
+            <div className='mt-1 absolute text-center'>
+                <Dropdown show={isExpanded} innerRef={menuRef}>
+                    <Dropdown.Separator />
+                    <Dropdown.Button className='text-left' onClick={() => setManageDataModalType('import')}>
+                        <span className='icon-base mr-1 align-bottom'>file_download</span> Import
+                    </Dropdown.Button>
+                    <Dropdown.Button className='text-left' onClick={() => setManageDataModalType('export')}>
+                        <span className='icon-base mr-1 align-bottom'>file_upload</span> Export
+                    </Dropdown.Button>
+                    <Dropdown.Separator />
+                    <Dropdown.Button className='text-left' onClick={() => setManageDataModalType('reset')}>
+                        <span className='icon-outline text-base mr-1 align-bottom'>dangerous</span> Reset
+                    </Dropdown.Button>
+                </Dropdown>
+            </div>
+        </div>
+    );
+}
+
+function CollapsedMenu({ setManageDataModalType }) {
+    return (
+        <React.Fragment key='dataManagement'>
+            <button
+                key='import'
+                className='text-primary bg-hover py-1 text-left'
+                onClick={() => setManageDataModalType('import')}
+                type='button'
+            >
+                <span className='text-primary-alt icon-lg inline align-middle mr-1'>file_download</span>
+                <p className='h-4 inline pl-1 font-sans-alt'>Import / Sync</p>
+            </button>
+            <button
+                key='export'
+                className='text-primary bg-hover py-1 text-left'
+                onClick={() => setManageDataModalType('export')}
+                type='button'
+            >
+                <span className='text-primary-alt icon-lg inline align-middle mr-1'>file_upload</span>
+                <p className='h-4 inline pl-1 font-sans-alt'>Export data</p>
+            </button>
+            <button
+                key='reset'
+                className='text-primary bg-hover py-1 text-left'
+                onClick={() => setManageDataModalType('reset')}
+                type='button'
+            >
+                <span className='text-primary-alt icon-lg inline align-middle mr-1'>dangerous</span>
+                <p className='h-4 inline pl-1 font-sans-alt'>Reset all data</p>
+            </button>
+        </React.Fragment>
+    );
+}
+
+export default { NavBarItem, CollapsedMenu };
