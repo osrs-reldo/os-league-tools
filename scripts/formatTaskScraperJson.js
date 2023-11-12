c661654b9af114d9dcb17e6b7857930cb60e5f4f
chaiinchomp <chaiinchomp@gmail.com>
2023-11-12 14:11:49 -0800

    Update wiki task scraper

--- scripts/formatTaskScraperJson.js
+++ scripts/formatTaskScraperJson.js
@@ -2,51 +2,57 @@ const taskMapper = require('./taskMapper');
 const fs = require('fs');
 const fetch = require('node-fetch');
 
+// TODO:
+// - ids are currently unstable and are regenerated each time the data is scraped
+// - categories temporarily disabled, mapper values need to be manually updated
+// - prereqs should have a mapper similar to categories
+
 // we want to keep enums from being expanded, so everything is generated as
 // strings to prevent them from being interpreted as references
 
 function formatTasks() {
-    const writeStream = fs.createWriteStream('./src/data/tasks.js');
-    writeStream.write("import { CATEGORY } from './categories';\n");
-    writeStream.write("import { DIFFICULTY } from './constants';\n\n");
-    writeStream.write('export default {\n    ');
-    fetchTaskJson().then(tasks => {
-        tasks.forEach(task => {
-            const { category, subcategory } = taskMapper.toCategories(task);
-            writeStream.write(`'${task.id}': {\n        `);
-            writeStream.write(`id: '${task.id}',\n        `);
-            writeStream.write(`label: ${JSON.stringify(task.name)},\n        `);
-            writeStream.write(`description: ${JSON.stringify(task.description)},\n        `);
-            writeStream.write(`skillReqs: ${formatSkillReqs(task.skills)},\n        `);
-            writeStream.write(`difficulty: DIFFICULTY.${task.tier.toUpperCase()},\n        `);
-            writeStream.write(`category: ${category && `CATEGORY.${category}`},\n        `);
-            writeStream.write(
-                `subcategory: ${subcategory && `CATEGORY.${category}.subcategories.${subcategory}`},\n    `
-            );
-            writeStream.write('},\n    ');
-        });
-        writeStream.write('};');
-        console.log('Successfully exported tasks to /src/data/tasks.js.');
+  const writeStream = fs.createWriteStream('./src/data/tasks.js');
+  writeStream.write("import { CATEGORY } from './categories';\n");
+  writeStream.write("import { DIFFICULTY } from './constants';\n\n");
+  writeStream.write("export const REGION_ANY = 'Any';\n\n");
+  writeStream.write('export default {\n  ');
+  fetchTaskJson().then(tasks => {
+    tasks.forEach((task, index) => {
+      // const { category, subcategory } = taskMapper.toCategories(task);
+      writeStream.write(`'${index}': {\n    `);
+      writeStream.write(`id: '${index}',\n    `);
+      writeStream.write(`label: ${JSON.stringify(task.name)},\n    `);
+      writeStream.write(`description: ${JSON.stringify(task.description)},\n    `);
+      writeStream.write(`skillReqs: ${formatSkillReqs(task.skills)},\n    `);
+      writeStream.write(`regions: [${JSON.stringify(task.area)}],\n    `);
+      writeStream.write(`difficulty: DIFFICULTY.${task.tier.toUpperCase()},\n    `);
+      writeStream.write(`category: CATEGORY.OTHER,\n    `);
+      writeStream.write(`subcategory: CATEGORY.OTHER.subcategories.GENERAL,\n    `);
+      // writeStream.write(`category: ${category && `CATEGORY.${category}`},\n    `);
+      // writeStream.write(`subcategory: ${subcategory && `CATEGORY.${category}.subcategories.${subcategory}`},\n    `);
+      writeStream.write("prerequisite: '',\n  ");
+      writeStream.write('},\n  ');
     });
+    writeStream.write('};');
+    console.log('Successfully exported tasks to /src/data/tasks.js.');
+  });
 }
 
 async function fetchTaskJson() {
-    let tasks;
-    await fetch('https://raw.githubusercontent.com/osrs-reldo/task-json-store/main/json/league3_tasks.json').then(
-        res => {
-            tasks = res.json();
-        }
-    );
-    return tasks;
+  let tasks;
+  await fetch('https://raw.githubusercontent.com/osrs-reldo/task-json-store/main/json/league4_tasks.json').then(res => {
+    tasks = res.json();
+  });
+  return tasks;
 }
 
 function formatSkillReqs(skillReqs) {
-    return stringifyArray(skillReqs.map(({ skill, level }) => `{skill: '${skill}',level: ${parseInt(level)}}`));
+  return stringifyArray(skillReqs.map(({ skill, level }) => `{skill: '${skill}',level: ${parseInt(level)}}`));
 }
 
 function stringifyArray(arr) {
-    // because JSON.stringify does annoying things like escaping quotes
-    return `[${arr.join(',\n')}]`;
+  // because JSON.stringify does annoying things like escaping quotes
+  return `[${arr.join(',\n')}]`;
 }
 
 formatTasks();
