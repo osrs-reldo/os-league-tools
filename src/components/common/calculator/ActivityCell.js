9fd6943bd34b340d24d6dc2c66bbc7eb9f5cb8af
chaiinchomp <chaiinchomp@gmail.com>
2023-11-12 11:42:23 -0800

    Show equilibrium info tooltip on calculators

--- src/components/common/calculator/ActivityCell.js
+++ src/components/common/calculator/ActivityCell.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
 
 export default function ActivityCell({ row, value }) {
   return (
     <div className='flex items-center py-2 h-full'>
       <img className='mx-2' src={row.original.icon} alt={value} />
       <div className='flex flex-col'>
-        <p>{value}</p>
+        <p>
+          {value} <EquilibriumTooltip activityId={row.original.id} equilibriumInfo={row.original.equilibriumInfo} />
+        </p>
         <p className='text-sm italic'>{row.original.subtitle}</p>
       </div>
     </div>
+  );
+}
+
+function EquilibriumTooltip({ activityId, equilibriumInfo }) {
+  if (!equilibriumInfo) {
+    return null;
+  }
+
+  return (
+    <>
+      <span className='icon-sm' data-tip data-for={`equilibrium-${activityId}`}>
+        balance
+      </span>
+      <ReactTooltip id={`equilibrium-${activityId}`}>For equilibrium: {equilibriumInfo}</ReactTooltip>
+    </>
   );
 }
