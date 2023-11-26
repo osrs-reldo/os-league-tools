002ff67ee00d62a69eccabf516741790ac67a1b2
chaiinchomp <chaiinchomp@gmail.com>
2023-11-26 10:18:53 -0800

    Only display boss hiscores for bosses in your regions

--- src/components/BossesPanel.js
+++ src/components/BossesPanel.js
@@ -2,10 +2,19 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import ReactTooltip from 'react-tooltip';
 import { BOSSES } from '../data/constants';
+import { NONE_REGION_ID, regionsById } from '../data/regions';
 
 export default function BossesPanel() {
   const hiscores = useSelector(state => state.character.hiscoresCache.data?.bosses);
-  const emptyTilesToRender = Object.values(BOSSES).length % 4;
+  const regionState = useSelector(state => state.unlocks.regions);
+
+  const unlockedRegionNames = new Set(
+    regionState.filter(regionId => regionId !== NONE_REGION_ID).map(regionId => regionsById[regionId].label)
+  );
+  const filteredBosses = Object.values(BOSSES).filter(bossData =>
+    bossData.regions.every(regionName => unlockedRegionNames.has(regionName))
+  );
+  const emptyTilesToRender = 4 - (filteredBosses.length % 4);
   const emptyTiles = [];
   for (let i = 0; i < emptyTilesToRender; i++) {
     emptyTiles.push(i);
@@ -13,7 +22,7 @@ export default function BossesPanel() {
 
   return (
     <div className='grid grid-cols-4 gap-px w-fit bg-subdued'>
-      {Object.values(BOSSES).map(boss => (
+      {filteredBosses.map(boss => (
         <BossTile key={boss.label} boss={boss} bossHiscores={hiscores ? hiscores[boss.hiscoresName] : {}} />
       ))}
       {emptyTiles.map(i => (
