666f0338a130a86a90ffa7ca9896cb7fe7e701a2
dmills94 <davidj-mills@live.co.uk>
2024-11-26 23:13:57 +0000

    fix auto unlock quest list not showing on region unlock modal

--- src/util/getAllQuestPrereqs.js
+++ src/util/getAllQuestPrereqs.js
@@ -2,13 +2,17 @@ import { questsById } from '../data/quests';
 
 function traverseQuestsRecursive(questId, callback) {
   const { prereqs } = questsById[questId];
-  prereqs.map(id => traverseQuestsRecursive(id, callback));
+  prereqs.forEach(id => {
+    callback(id);
+    traverseQuestsRecursive(id, callback);
+  });
 }
 
 export default function getAllQuestPrereqs(questIds) {
   const allPrereqs = new Set();
-  for (const questId of questIds) {
+  questIds.forEach(questId => {
+    allPrereqs.add(questId);
     traverseQuestsRecursive(questId, id => allPrereqs.add(id));
-  }
+  });
   return Array.from(allPrereqs);
 }
