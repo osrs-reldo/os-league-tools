a056cd4fd11e566b3b5caef79d525177dc1ec56c
dmills94 <davidj-mills@live.co.uk>
2024-11-26 15:11:21 +0000

    add ability to select a reloaded relic

--- src/hooks/useEquilibrium.js
+++ src/hooks/useEquilibrium.js
@@ -5,7 +5,7 @@ export default function useEquilibrium() {
   const hiscoresState = useSelector(state => state.character.hiscoresCache);
   const relicsState = useSelector(state => state.unlocks.relics);
   const [totalLevel, setTotalLevel] = useState(hiscoresState.data?.skills?.overall?.level ?? 0);
-  const [enabled, setEnabled] = useState(relicsState[5] === 0);
+  const [enabled, setEnabled] = useState(relicsState[3] === 2);
   const bonusExpPerAction = totalLevel * 0.1;
 
   const getBonusExp = equilibriumTriggersPerAction => {
